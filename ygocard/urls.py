58abdd5b4245ca716d6b6b8bb53d4718467fddd6
Ivan Gajovic <mt.ivangajovic@gmail.com>
2017-09-25 14:56:41 +0200

    Added authentication, delete

--- ygocard/urls.py
+++ ygocard/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^card/(?P<pk>\d+)/$', views.card_detail, name='card_detail'),
     url(r'^card/new/$', views.add_new_card, name='add_new_card'),
     url(r'^card/(?P<pk>\d+)/edit/$', views.card_edit, name='card_edit'),
+    url(r'^card/(?P<pk>\d+)/remove/$', views.card_remove, name='card_remove'),
 ]
--- ygocard/views.py
+++ ygocard/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, get_list_or_404
+from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 from .models import Card, CardType, MonsterType, MonsterType1, MonsterType2, MonsterAbility, SpellTrapType, CardRarity
 from .forms import CardForm, CardTypeForm, MonsterTypeForm, MonsterType1Form, MonsterType2Form, MonsterAbilityForm, SpellTrapTypeForm, CardRarityForm
@@ -15,6 +16,7 @@ def card_detail(request, pk):
     return render(request, 'ygocard/card_detail.html', {'card': card})
 
 
+@login_required
 def add_new_card(request):
     # Check to see if a POST has been submitted
     if request.method == "POST":
@@ -57,26 +59,19 @@ def add_new_card(request):
     return render(request, 'ygocard/card_edit.html', {'form': form, 'form_card_type': form_card_type, 'form_monster_type': form_monster_type, 'form_monster_type1': form_monster_type1, 'form_monster_type2': form_monster_type2, 'form_monster_ability': form_monster_ability, 'form_spell_trap': form_spell_trap, 'form_card_rarity': form_card_rarity})
 
 
-
+@login_required
 def card_edit(request, pk):
     card = get_object_or_404(Card, pk=pk)
-    c_type = get_object_or_404(CardType)
-    m_type = get_object_or_404(MonsterType)
-    m_type1 = get_object_or_404(MonsterType1)
-    m_type2 = get_object_or_404(MonsterType2)
-    m_ability = get_object_or_404(MonsterAbility)
-    st_type = get_object_or_404(SpellTrapType)
-    c_rarity = get_object_or_404(CardRarity)
 
     if request.method == "POST":
         form = CardForm(request.POST, request.FILES, instance=card, prefix='crd')
-        form_card_type = CardTypeForm(request.POST, instance=c_type, prefix='ctyp')
-        form_monster_type = MonsterTypeForm(request.POST, instance=m_type, prefix='mtyp')
-        form_monster_type1 = MonsterType1Form(request.POST, instance=m_type1, prefix='mtyp1')
-        form_monster_type2 = MonsterType2Form(request.POST, instance=m_type2, prefix='mtyp2')
-        form_monster_ability = MonsterAbilityForm(request.POST, instance=m_ability, prefix='mabt')
-        form_spell_trap = SpellTrapTypeForm(request.POST, instance=st_type, prefix='stt')
-        form_card_rarity = CardRarityForm(request.POST, instance=c_rarity, prefix='crrt')
+        form_card_type = CardTypeForm(request.POST, instance=card.card_type, prefix='ctyp')
+        form_monster_type = MonsterTypeForm(request.POST, instance=card.monster_type, prefix='mtyp')
+        form_monster_type1 = MonsterType1Form(request.POST, instance=card.monster_type_1, prefix='mtyp1')
+        form_monster_type2 = MonsterType2Form(request.POST, instance=card.monster_type_2, prefix='mtyp2')
+        form_monster_ability = MonsterAbilityForm(request.POST, instance=card.monster_ability, prefix='mabt')
+        form_spell_trap = SpellTrapTypeForm(request.POST, instance=card.spell_trap_type, prefix='stt')
+        form_card_rarity = CardRarityForm(request.POST, instance=card.card_rarity, prefix='crrt')
 
         if form.is_valid() and form_card_type.is_valid() and form_monster_type.is_valid() and form_monster_type1.is_valid() and form_monster_type2.is_valid() and form_monster_ability.is_valid() and form_spell_trap.is_valid() and form_card_rarity.is_valid():
             card = form.save(commit=False)
@@ -93,12 +88,19 @@ def card_edit(request, pk):
             return redirect('card_detail', pk=card.pk)
     else:
         form = CardForm(instance=card, prefix='crd')
-        form_card_type = CardTypeForm(instance=c_type, prefix='ctyp')
-        form_monster_type = MonsterTypeForm(instance=m_type, prefix='mtyp')
-        form_monster_type1 = MonsterType1Form(instance=m_type1, prefix='mtyp1')
-        form_monster_type2 = MonsterType2Form(instance=m_type2, prefix='mtyp2')
-        form_monster_ability = MonsterAbilityForm(instance=m_ability, prefix='mabt')
-        form_spell_trap = SpellTrapTypeForm(instance=st_type, prefix='stt')
-        form_card_rarity = CardRarityForm(instance=c_rarity, prefix='crrt')
+        form_card_type = CardTypeForm(instance=card.card_type, prefix='ctyp')
+        form_monster_type = MonsterTypeForm(instance=card.monster_type, prefix='mtyp')
+        form_monster_type1 = MonsterType1Form(instance=card.monster_type_1, prefix='mtyp1')
+        form_monster_type2 = MonsterType2Form(instance=card.monster_type_2, prefix='mtyp2')
+        form_monster_ability = MonsterAbilityForm(instance=card.monster_ability, prefix='mabt')
+        form_spell_trap = SpellTrapTypeForm(instance=card.spell_trap_type, prefix='stt')
+        form_card_rarity = CardRarityForm(instance=card.card_rarity, prefix='crrt')
 
     return render(request, 'ygocard/card_edit.html', {'form': form, 'form_card_type': form_card_type, 'form_monster_type': form_monster_type, 'form_monster_type1': form_monster_type1, 'form_monster_type2': form_monster_type2, 'form_monster_ability': form_monster_ability, 'form_spell_trap': form_spell_trap, 'form_card_rarity': form_card_rarity})
+
+
+@login_required
+def card_remove(request, pk):
+    card = get_object_or_404(Card, pk=pk)
+    card.delete()
+    return redirect('card_list')
