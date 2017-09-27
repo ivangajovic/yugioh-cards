ff51aa4a6ca3d9b76e8739a86ea9c9d1747f5199
Ivan Gajovic <mt.ivangajovic@gmail.com>
2017-09-27 10:52:53 +0200

    Added changes required for deployment to Heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -134,5 +134,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 LOGIN_REDIRECT_URL = '/'
 
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max=500)
+db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
